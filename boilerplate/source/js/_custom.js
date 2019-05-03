efc1a53a1bce3889ebd3a129a7f7fb8f88d13555
LuuThom <thom0702>
2019-05-03 16:42:06 +0700

    add section

--- boilerplate/source/js/_custom.js
+++ boilerplate/source/js/_custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('.owl-carousel').owlCarousel({
+  $('.js-product').owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
@@ -15,6 +15,25 @@ $(document).ready(function () {
       },
       1000: {
         items: 3
+      }
+    }
+  });
+  $('.js-meet-team').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 4
+      },
+      1000: {
+        items: 4
       }
     }
   });
